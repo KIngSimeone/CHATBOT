f8ba80992b66aa5d61834386ed9e5f328c77f47b
KingSimeone <simeoneumoh@outlook.com>
2020-02-24 09:55:08 +0100

    add greetings

--- chatty.py
+++ chatty.py
@@ -29,4 +29,12 @@ def LemNormalize(text):
 
 
 
-GREETING_INPUT
+GREETING_INPUTS = ("hello", "hi", "greetings", "sup", "what's up", "hey",)
+
+GREETING_RESPONSES = ["hi","hey", "*nods*", "hi there", "hello", "I am glad! You are talking to me"
+
+def greeting(sentence):
+
+    for word in sentence.split():
+        if word.lower() in GREETING_INPUTS:
+            return random.choice(GREETING_RESPONSES)
