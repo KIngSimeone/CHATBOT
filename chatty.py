f57af5cb19d82b0a75e9459fc51d2664123fa762
KingSimeone <simeoneumoh@outlook.com>
2020-02-24 12:12:48 +0100

    SJR Complete

--- chatty.py
+++ chatty.py
@@ -4,13 +4,13 @@ import random
 import string
 
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.metrics.paiwise import cosine_similarity
+from sklearn.metrics.pairwise import cosine_similarity
 
 
 
 f=open('chatbot.txt', 'r', errors = 'ignore')
 
-raw=f.read
+raw=f.read()
 raw = raw.lower()
 
 nltk.download('punkt')
@@ -19,7 +19,7 @@ nltk.download('wordnet')
 sent_tokens = nltk.sent_tokenize(raw)
 word_tokens = nltk.word_tokenize(raw)
 
-lemmer = nltk.stem.WordNetLemmatize()
+lemmer = nltk.stem.WordNetLemmatizer()
 
 def LemTokens(tokens):
     return [lemmer.lemmatize(token)for token in tokens]
@@ -34,7 +34,7 @@ def LemNormalize(text):
 
 GREETING_INPUTS = ("hello", "hi", "greetings", "sup", "what's up", "hey",)
 
-GREETING_RESPONSES = ["hi","hey", "*nods*", "hi there", "hello", "I am glad! You are talking to me"
+GREETING_RESPONSES = ["hi","hey", "*nods*", "hi there", "hello", "I am glad! You are talking to me"]
 
 def greeting(sentence):
 
@@ -54,11 +54,37 @@ def response(user_response):
     idx=vals.argsort() [0] [-2]
     flat = vals.flatten()
     flat.sort()
-    reg_tfidf = flat[-2]
+    req_tfidf = flat[-2]
 
     if(req_tfidf==0):
         robo_response=robo_response+"I am sorry! I don't understand you"
-        retrun robo_response
+        return robo_response
     else:
         robo_response = robo_response+sent_tokens[idx]
         return robo_response
+
+
+
+flag=True
+print("SJR:My name is SimeoneJr. I will answer your queries about Python programming language. If you want to exit, type bye!.")
+
+while(flag==True):
+    user_response = input()
+    user_response = user_response.lower()
+    if(user_response!='bye'):
+        if (user_response=='thanks' or user_response =='thank you'):
+            flag=False
+            print("SJR: You are welcome..")
+        else:
+            if(greeting(user_response) !=None):
+                print("SJR: "+greeting(user_response))
+
+            else:
+                print("SJR: ", end="")
+                print(response(user_response))
+                sent_tokens.remove(user_response)
+
+    else:
+        flag=False
+        print("SJR: See ya mate.")
+
